565fee9943504be642a591c77efa79d4ed7976ba
ssulang <terry940513@naver.com>
2022-12-21 00:18:08 +0900

    1221

--- Dynamic Beat/src/dynamic_beat/DynamicBeat.java
+++ Dynamic Beat/src/dynamic_beat/DynamicBeat.java
@@ -7,7 +7,6 @@ import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseMotionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -19,27 +18,45 @@ public class DynamicBeat extends JFrame {
 	private Image screenImage;
 	private Graphics screenGraphic;
 
-	private Image introBackgound = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();;
-	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
-
-	
 	private ImageIcon exitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/exitButtonEntered.png"));
 	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/exitButtonBasic.png"));
+	private ImageIcon startButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/startButtonEntered.png"));
+	private ImageIcon startButtonBasicImage = new ImageIcon(Main.class.getResource("../images/startButtonBasic.png"));
+	private ImageIcon quitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/quitButtonEntered.png"));
+	private ImageIcon quitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/quitButtonBasic.png"));
+	private ImageIcon leftButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/leftButtonEntered.png"));
+	private ImageIcon leftButtonBasicImage = new ImageIcon(Main.class.getResource("../images/leftButtonBasic.png"));
+	private ImageIcon rightButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/rightButtonEntered.png"));
+	private ImageIcon rightButtonBasicImage = new ImageIcon(Main.class.getResource("../images/rightButtonBasic.png"));
+	
+	private Image titleImage = new ImageIcon(Main.class.getResource("../images/choicemusic.png")).getImage();
+	private Image selectedImage = new ImageIcon(Main.class.getResource("../images/Rewind Game Image.jpg"))
+			.getImage();
+	private Image background = new ImageIcon(Main.class.getResource("../images/introBackground(Title).jpg"))
+			.getImage();
+	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
+
 	private JButton exitButton = new JButton(exitButtonBasicImage);
+	private JButton startButton = new JButton(startButtonBasicImage);
+	private JButton quitButton = new JButton(quitButtonBasicImage);
+	private JButton leftButton = new JButton(leftButtonBasicImage);
+	private JButton rightButton = new JButton(rightButtonBasicImage);
 	
 	private int mouseX, mouseY;
+	
+	private boolean isMainScreen = false;
 	
 	public DynamicBeat() {
 		setUndecorated(true);
 		setTitle("Dynamic Beat");
 		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
-		setResizable(false);// 사이즈를 임의로 줄이는게 안됨
-		setLocationRelativeTo(null);// 화면이 컴퓨터 정중앙
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 창을끄면 프로그램 종료
-		setVisible(true);// 게임이 정상적이면 출력
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
 		setBackground(new Color(0, 0, 0, 0));
 		setLayout(null);
-		
+
 		exitButton.setBounds(1245, 0, 30, 30);
 		exitButton.setBorderPainted(false);
 		exitButton.setContentAreaFilled(false);
@@ -49,6 +66,8 @@ public class DynamicBeat extends JFrame {
 			public void mouseEntered(MouseEvent e) {
 				exitButton.setIcon(exitButtonEnteredImage);
 				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music("buttonEnteredMusic.mp3", false);
+				buttonEnteredMusic.start();
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
@@ -57,12 +76,136 @@ public class DynamicBeat extends JFrame {
 			}
 			@Override
 			public void mousePressed(MouseEvent e) {
-				System.exit(0); 
-			}		
+				Music buttonEnteredMusic = new Music("buttonPressedMusic.mp3", false);
+				buttonEnteredMusic.start();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+				System.exit(0);
+			}
 		});
-		
 		add(exitButton);
+		
+		startButton.setBounds(150, 550, 400, 100);
+		startButton.setBorderPainted(false);
+		startButton.setContentAreaFilled(false);
+		startButton.setFocusPainted(false);
+		startButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				startButton.setIcon(startButtonEnteredImage);
+				startButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music("buttonEnteredMusic.mp3", false);
+				buttonEnteredMusic.start();
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				startButton.setIcon(startButtonBasicImage);
+				startButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonEnteredMusic = new Music("buttonPressedMusic.mp3", false);
+				buttonEnteredMusic.start();
+				startButton.setVisible(false);
+				quitButton.setVisible(false);
+				leftButton.setVisible(true);
+				rightButton.setVisible(true);
+				background = new ImageIcon(Main.class.getResource("../images/mainBackground.jpg"))
+						.getImage();
+				isMainScreen = true;
+			}
+		});
+		add(startButton);
+		
+		quitButton.setBounds(750, 550, 400, 100);
+		quitButton.setBorderPainted(false);
+		quitButton.setContentAreaFilled(false);
+		quitButton.setFocusPainted(false);
+		quitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				quitButton.setIcon(quitButtonEnteredImage);
+				quitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music("buttonEnteredMusic.mp3", false);
+				buttonEnteredMusic.start();
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				quitButton.setIcon(quitButtonBasicImage);
+				quitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonEnteredMusic = new Music("buttonPressedMusic.mp3", false);
+				buttonEnteredMusic.start();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+				System.exit(0);
+			}
+		});
+		add(quitButton);
+		
+		leftButton.setVisible(false);
+		leftButton.setBounds(80, 310, 60, 60);
+		leftButton.setBorderPainted(false);
+		leftButton.setContentAreaFilled(false);
+		leftButton.setFocusPainted(false);
+		leftButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				leftButton.setIcon(leftButtonEnteredImage);
+				leftButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music("buttonEnteredMusic.mp3", false);
+				buttonEnteredMusic.start();
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				leftButton.setIcon(leftButtonBasicImage);
+				leftButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonEnteredMusic = new Music("buttonPressedMusic.mp3", false);
+				buttonEnteredMusic.start();
+				// 왼쪽 버튼 이벤트
+			}
+		});
+		add(leftButton);
 
+		rightButton.setVisible(false);
+		rightButton.setBounds(1200, 310, 60, 60);
+		rightButton.setBorderPainted(false);
+		rightButton.setContentAreaFilled(false);
+		rightButton.setFocusPainted(false);
+		rightButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				rightButton.setIcon(rightButtonEnteredImage);
+				rightButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music("buttonEnteredMusic.mp3", false);
+				buttonEnteredMusic.start();
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				rightButton.setIcon(rightButtonBasicImage);
+				rightButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonEnteredMusic = new Music("buttonPressedMusic.mp3", false);
+				buttonEnteredMusic.start();
+				// 오른쪽 버튼 이벤트
+			}
+		});
+		add(rightButton);
+		
+		
 		menuBar.setBounds(0, 0, 1280, 30);
 		menuBar.addMouseListener(new MouseAdapter() {
 			@Override
@@ -81,9 +224,7 @@ public class DynamicBeat extends JFrame {
 		});
 		add(menuBar);
 
-
-
-		Music introMusic = new Music("../music/introMusic.mp3", true);
+		Music introMusic = new Music("introMusic.mp3", true);
 		introMusic.start();
 	}
 
@@ -95,8 +236,17 @@ public class DynamicBeat extends JFrame {
 	}
 
 	public void screenDraw(Graphics g) {
-		g.drawImage(introBackgound, 0, 0, null);
+		g.drawImage(background, 0, 0, null);
+		if(isMainScreen)
+		{	
+			g.drawImage(selectedImage, 0, 0, null);
+			g.drawImage(titleImage, 380, 50, null);
+		}
 		paintComponents(g);
 		this.repaint();
 	}
+
 }
+
+
+
--- Dynamic Beat/src/dynamic_beat/Music.java
+++ Dynamic Beat/src/dynamic_beat/Music.java
@@ -6,34 +6,38 @@ import java.io.FileInputStream;
 
 import javazoom.jl.player.Player;
 
-public class Music extends Thread{
-
+public class Music extends Thread {
+	
 	private Player player;
 	private boolean isLoop;
 	private File file;
 	private FileInputStream fis;
 	private BufferedInputStream bis;
-
+	
 	public Music(String name, boolean isLoop) {
 		try {
-			file = new File(Main.class.getResource("../music/introMusic.mp3").toURI());
+			this.isLoop = isLoop;
+			file = new File(Main.class.getResource("../music/" + name).toURI());
 			fis = new FileInputStream(file);
 			bis = new BufferedInputStream(fis);
 			player = new Player(bis);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
+	
 	public int getTime() {
 		if (player == null)
 			return 0;
 		return player.getPosition();
 	}
+	
 	public void close() {
 		isLoop = false;
 		player.close();
 		this.interrupt();
 	}
+	
 	@Override
 	public void run() {
 		try {
@@ -41,10 +45,12 @@ public class Music extends Thread{
 				player.play();
 				fis = new FileInputStream(file);
 				bis = new BufferedInputStream(fis);
-				player = new Player(bis);//음악 무한반복
-			}while(isLoop);
-		}catch(Exception e) {
+				player = new Player(bis);
+			} while (isLoop);
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
+
 }
+
